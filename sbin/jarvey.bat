425474794abaf9805982a8c35e1051b0edfb1f91
kwlee <kwlee@etri.re.kr>
2023-12-30 01:01:22 +0900

    .

--- sbin/jarvey.bat
+++ sbin/jarvey.bat
@@ -1,5 +1,5 @@
 @echo off
 
 java -Dlog4j.configurationFile=D:\Dropbox\development\jarvey\jarvey.streams\log4j2.xml ^
--cp %JARVEY_STREAMS_HOME%/sbin/jarvey.streams-0.1.0-all.jar ^
+-cp %JARVEY_STREAMS_HOME%/sbin/jarvey.streams-2023.12.29-all.jar ^
 jarvey.streams.assoc.tool.JarveyStreamCommandsMain %*

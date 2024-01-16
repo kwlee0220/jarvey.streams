4964348da69236fbfe01965477a24f6d4d40b574
'kwlee' <'kwlee@etri.re.kr'>
2024-01-16 13:06:45 +0900

    .

--- sbin/jarvey.sh
+++ sbin/jarvey.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 
-java -Dlog4j.configurationFile=D:\Dropbox\development\jarvey\jarvey.streams\log4j2.xml \
--cp %JARVEY_STREAMS_HOME%/sbin/jarvey.streams-2023.12.29-all.jar \
+java -Dlog4j.configurationFile=%JARVEY_STREAMS_HOME%/log4j2.xml \
+-cp %JARVEY_STREAMS_HOME%/sbin/jarvey.streams.jar \
 jarvey.streams.assoc.tool.JarveyStreamCommandsMain "$@"
